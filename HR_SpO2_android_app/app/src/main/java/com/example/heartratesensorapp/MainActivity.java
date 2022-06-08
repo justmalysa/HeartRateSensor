4bd3aff6f8e5ae072f90e1ebbb4e7b0935776600
maciejsudo <maciejsudol99@gmail.com>
2022-06-08 17:08:42 +0200

    initial commit

---
+++ HR_SpO2_android_app/app/src/main/java/com/example/heartratesensorapp/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.heartratesensorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    BluetoothAdapter mBluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnONOFF = (Button) findViewById(R.id.btnONOFF);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        btnONOFF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enable_disable_BT();
+            }
+        });
+
+    }
+
+    public void enable_disable_BT()
+    {
+        if(mBluetoothAdapter==null)
+        {
+            Log.d(TAG,"enable_disable_BT: Does not have BT capabilities. ");
+        }
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBTIntent = new Intent (BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1,BTIntent);
+        }
+        if(mBluetoothAdapter.isEnabled())
+        {
+            mBluetoothAdapter.disable();
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1,BTIntent);
+
+
+
+        }
+    }
+}
