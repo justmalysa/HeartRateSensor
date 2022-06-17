a2da40e4861122724fa2b2c28e3c13183cc33ffe
justmalysa <justynamalysa98@gmail.com>
2022-06-17 15:14:27 +0200

    Change the USART baud rate to 115200

--- HR_SpO2_sensor/Core/Src/main.c
+++ HR_SpO2_sensor/Core/Src/main.c
@@ -49,10 +49,8 @@ TIM_HandleTypeDef htim3;
 UART_HandleTypeDef huart1;
 USART_HandleTypeDef husart2;
 
-static uint8_t rx_data;
-
 /* USER CODE BEGIN PV */
-
+static uint8_t rx_data;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -355,7 +353,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 9600;
+  huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
